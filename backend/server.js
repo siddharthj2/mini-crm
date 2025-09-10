1f8757408960941f1a51f7dadf1a72deef5b5886
siddharthj2 <siddharthjindal456@gmail.com>
2025-09-10 11:08:20 +0530

    added logs for query

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const connectDB=require("./config/mongo");
 const customerRoutes=require("./routes/customers");
 const orderRoutes=require("./routes/orders")
 const campaignRoutes=require("./routes/campaigns")
+const logsRoutes=require("./routes/logs")
 
 dotenv.config();
 const app=express();
@@ -13,6 +14,7 @@ app.use(express.json())
 app.use("/api/customer",customerRoutes);
 app.use("/api/order",orderRoutes);
 app.use("/api/campaign",campaignRoutes);
+app.use("/api/logs",logsRoutes);
 
 connectDB();
 app.listen(port,()=>console.log(`server is running on port ${port}`));
